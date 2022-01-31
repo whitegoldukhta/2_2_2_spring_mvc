a38bc8af4ae73c94b1a6f06c33966b21e47485d0
whitegoldukhta <james.99@list.ru>
2022-01-31 18:15:27 +0200

    web

--- PP_2_2_2_spring-mvc/src/main/java/web/service/CarServiceImpl.java
+++ PP_2_2_2_spring-mvc/src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,13 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class CarServiceImpl implements CarService {
     private List<Car> cars;
 
