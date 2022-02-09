ee0f056f617093278227b6304f77aceb381e6677
whitegoldukhta <james.99@list.ru>
2022-02-09 16:03:44 +0200

    web

--- PP_2_2_2_spring-mvc/src/main/java/web/controller/CarsController.java
+++ PP_2_2_2_spring-mvc/src/main/java/web/controller/CarsController.java
@@ -1,10 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.List;
@@ -12,10 +14,12 @@ import java.util.List;
 @Controller
 public class CarsController {
 
+    @Autowired
+    private CarService carService;
+
     @GetMapping(value = "/cars")
     public String cars(@RequestParam(value = "count", required = false, defaultValue = "5") int countCar, Model model) {
-        List<Car> carList = new CarServiceImpl().listCars(countCar);
-        model.addAttribute("list", carList);
+        model.addAttribute("list", carService.listCars(countCar));
         return "cars";
     }
 
